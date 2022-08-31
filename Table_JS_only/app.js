96900ab34505e98e41ea001472bf07850d5a0ef4
Muhammad Mudabbir <106159197+MuhammadMudabbirMunaf@users.noreply.github.com>
2022-09-01 01:10:01 +0500

    Add files via upload

---
+++ Table_JS_only/app.js
@@ -0,0 +1,231 @@
+// var attClass = document.getElementById("table");
+// .appendChild(tableCreation);
+// var newAtt = attClass.setAttribute("class", "table");
+
+var bodyTag = document.getElementById("bodyNode");
+bodyTag.appendChild(containerDiv);
+// Div Creation
+var containerDiv = document.createElement("DIV");
+
+// Table First Row Creation and Child Appending
+var tableCreation = document.createElement("TABLE");
+tableCreation.style.border = "2px solid black";
+containerDiv.appendChild(tableCreation);
+var tableRow = document.createElement("TR");
+tableCreation.appendChild(tableRow);
+var tableData = document.createElement("TD");
+tableRow.appendChild(tableData);
+var textNode = document.createTextNode("August 2022");
+tableData.appendChild(textNode);
+
+// Table Second Row Creation and Child Appending
+var tableRow2 = document.createElement("TR");
+
+var tableData2_1 = document.createElement("TD");
+var tableData2_2 = document.createElement("TD");
+var tableData2_3 = document.createElement("TD");
+var tableData2_4 = document.createElement("TD");
+var tableData2_5 = document.createElement("TD");
+var tableData2_6 = document.createElement("TD");
+var tableData2_7 = document.createElement("TD");
+
+// Appending TD in TR
+tableRow2.appendChild(tableData2_1);
+tableRow2.appendChild(tableData2_2);
+tableRow2.appendChild(tableData2_3);
+tableRow2.appendChild(tableData2_4);
+tableRow2.appendChild(tableData2_5);
+tableRow2.appendChild(tableData2_6);
+tableRow2.appendChild(tableData2_7);
+
+var textNode2_1 = document.createTextNode("Monday");
+var textNode2_2 = document.createTextNode("Tuesday");
+var textNode2_3 = document.createTextNode("Wednesday");
+var textNode2_4 = document.createTextNode("Thursday");
+var textNode2_5 = document.createTextNode("Friday");
+var textNode2_6 = document.createTextNode("Saturday");
+var textNode2_7 = document.createTextNode("Sunday");
+
+// Appending TextNode in TR
+tableData2_1.appendChild(textNode2_1);
+tableData2_2.appendChild(textNode2_2);
+tableData2_3.appendChild(textNode2_3);
+tableData2_4.appendChild(textNode2_4);
+tableData2_5.appendChild(textNode2_5);
+tableData2_6.appendChild(textNode2_6);
+tableData2_7.appendChild(textNode2_7);
+
+// Table Third Row Creation and Child Appending
+var tableRow3 = document.createElement("TR");
+
+var tableData3_1 = document.createElement("TD");
+var tableData3_2 = document.createElement("TD");
+var tableData3_3 = document.createElement("TD");
+var tableData3_4 = document.createElement("TD");
+var tableData3_5 = document.createElement("TD");
+var tableData3_6 = document.createElement("TD");
+var tableData3_7 = document.createElement("TD");
+
+// Appending TD in TR
+tableRow3.appendChild(tableData3_1);
+tableRow3.appendChild(tableData3_2);
+tableRow3.appendChild(tableData3_3);
+tableRow3.appendChild(tableData3_4);
+tableRow3.appendChild(tableData3_5);
+tableRow3.appendChild(tableData3_6);
+tableRow3.appendChild(tableData3_7);
+
+var textNode3_1 = document.createTextNode("01");
+var textNode3_2 = document.createTextNode("02");
+var textNode3_3 = document.createTextNode("03");
+var textNode3_4 = document.createTextNode("04");
+var textNode3_5 = document.createTextNode("05");
+var textNode3_6 = document.createTextNode("06");
+var textNode3_7 = document.createTextNode("07");
+
+// Appending TextNode in TR
+tableData3_1.appendChild(textNode3_1);
+tableData3_2.appendChild(textNode3_2);
+tableData3_3.appendChild(textNode3_3);
+tableData3_4.appendChild(textNode3_4);
+tableData3_5.appendChild(textNode3_5);
+tableData3_6.appendChild(textNode3_6);
+tableData3_7.appendChild(textNode3_7);
+
+
+
+// Table Fourth Row Creation and Child Appending
+var tableRow4 = document.createElement("TR");
+
+var tableData4_1 = document.createElement("TD");
+var tableData4_2 = document.createElement("TD");
+var tableData4_3 = document.createElement("TD");
+var tableData4_4 = document.createElement("TD");
+var tableData4_5 = document.createElement("TD");
+var tableData4_6 = document.createElement("TD");
+var tableData4_7 = document.createElement("TD");
+
+// Appending TD in TR
+tableRow4.appendChild(tableData4_1);
+tableRow4.appendChild(tableData4_2);
+tableRow4.appendChild(tableData4_3);
+tableRow4.appendChild(tableData4_4);
+tableRow4.appendChild(tableData4_5);
+tableRow4.appendChild(tableData4_6);
+tableRow4.appendChild(tableData4_7);
+
+var textNode4_1 = document.createTextNode("08");
+var textNode4_2 = document.createTextNode("09");
+var textNode4_3 = document.createTextNode("10");
+var textNode4_4 = document.createTextNode("11");
+var textNode4_5 = document.createTextNode("12");
+var textNode4_6 = document.createTextNode("13");
+var textNode4_7 = document.createTextNode("14");
+
+// Appending TextNode in TR
+tableData4_1.appendChild(textNode4_1);
+tableData4_2.appendChild(textNode4_2);
+tableData4_3.appendChild(textNode4_3);
+tableData4_4.appendChild(textNode4_4);
+tableData4_5.appendChild(textNode4_5);
+tableData4_6.appendChild(textNode4_6);
+tableData4_7.appendChild(textNode4_7);
+
+
+
+// Table Fifth Row Creation and Child Appending
+var tableRow5 = document.createElement("TR");
+
+var tableData5_1 = document.createElement("TD");
+var tableData5_2 = document.createElement("TD");
+var tableData5_3 = document.createElement("TD");
+var tableData5_4 = document.createElement("TD");
+var tableData5_5 = document.createElement("TD");
+var tableData5_6 = document.createElement("TD");
+var tableData5_7 = document.createElement("TD");
+
+// Appending TD in TR
+tableRow5.appendChild(tableData5_1);
+tableRow5.appendChild(tableData5_2);
+tableRow5.appendChild(tableData5_3);
+tableRow5.appendChild(tableData5_4);
+tableRow5.appendChild(tableData5_5);
+tableRow5.appendChild(tableData5_6);
+tableRow5.appendChild(tableData5_7);
+
+var textNode5_1 = document.createTextNode("15");
+var textNode5_2 = document.createTextNode("16");
+var textNode5_3 = document.createTextNode("17");
+var textNode5_4 = document.createTextNode("18");
+var textNode5_5 = document.createTextNode("19");
+var textNode5_6 = document.createTextNode("20");
+var textNode5_7 = document.createTextNode("21");
+
+// Appending TextNode in TR
+tableData5_1.appendChild(textNode5_1);
+tableData5_2.appendChild(textNode5_2);
+tableData5_3.appendChild(textNode5_3);
+tableData5_4.appendChild(textNode5_4);
+tableData5_5.appendChild(textNode5_5);
+tableData5_6.appendChild(textNode5_6);
+tableData5_7.appendChild(textNode5_7);
+
+
+// Table Sixth Row Creation and Child Appending
+var tableRow6 = document.createElement("TR");
+
+var tableData6_1 = document.createElement("TD");
+var tableData6_2 = document.createElement("TD");
+var tableData6_3 = document.createElement("TD");
+var tableData6_4 = document.createElement("TD");
+var tableData6_5 = document.createElement("TD");
+var tableData6_6 = document.createElement("TD");
+var tableData6_7 = document.createElement("TD");
+
+// Appending TD in TR
+tableRow6.appendChild(tableData6_1);
+tableRow6.appendChild(tableData6_2);
+tableRow6.appendChild(tableData6_3);
+tableRow6.appendChild(tableData6_4);
+tableRow6.appendChild(tableData6_5);
+tableRow6.appendChild(tableData6_6);
+tableRow6.appendChild(tableData6_7);
+
+var textNode6_1 = document.createTextNode("22");
+var textNode6_2 = document.createTextNode("23");
+var textNode6_3 = document.createTextNode("24");
+var textNode6_4 = document.createTextNode("25");
+var textNode6_5 = document.createTextNode("26");
+var textNode6_6 = document.createTextNode("27");
+var textNode6_7 = document.createTextNode("28");
+
+// Appending TextNode in TR
+tableData6_1.appendChild(textNode6_1);
+tableData6_2.appendChild(textNode6_2);
+tableData6_3.appendChild(textNode6_3);
+tableData6_4.appendChild(textNode6_4);
+tableData6_5.appendChild(textNode6_5);
+tableData6_6.appendChild(textNode6_6);
+tableData6_7.appendChild(textNode6_7);
+
+
+// Table Seventh Row Creation and Child Appending
+var tableRow7 = document.createElement("TR");
+
+// Appending TD in TR
+var tableData7_1 = document.createElement("TD");
+var tableData7_2 = document.createElement("TD");
+var tableData7_3 = document.createElement("TD");
+
+tableRow7.appendChild(tableData7_1);
+tableRow7.appendChild(tableData7_2);
+tableRow7.appendChild(tableData7_3);
+
+var textNode7_1 = document.createTextNode("29");
+var textNode7_2 = document.createTextNode("30");
+var textNode7_3 = document.createTextNode("31");
+
+// Appending TextNode in TR
+tableData7_1.appendChild(textNode7_1);
+tableData7_2.appendChild(textNode7_2);
+tableData7_3.appendChild(textNode7_3);
